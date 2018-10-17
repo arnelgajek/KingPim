a06f75f8a120c0c18e78687a85822226f4eefc35
Arnel Gajek <gaar1719@student.ju.se>
2018-10-17 11:34:15 +0200

    Created all the Controllers that will be needed in the project at this point.

---
+++ KingPim.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingPim.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // TODO: Connect to database with private property and private cunstructor.
+
+        public IActionResult Index()
+        {
+            // TODO: Where should this one be returned?
+            return View();
+        }
+
+        // TODO: GetAll
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}

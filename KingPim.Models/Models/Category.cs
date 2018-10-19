583216e935b3406431195be2e39efb80e15274a0
Arnel Gajek <gaar1719@student.ju.se>
2018-10-19 10:43:07 +0200

    Added Order Column to the models, still not working because the base class ReadOnlyAttributes come first. Save this for some other time.

--- KingPim.Models/Models/Category.cs
+++ KingPim.Models/Models/Category.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace KingPim.Models.Models
 {
     public class Category : ReadOnlyAttribute
     {
+        [Column(Order = 0)]
         public int Id { get; set; }
+        [Column(Order = 1)]
         public string Name { get; set; }
         public virtual List<SubCategory> SubCategories { get; set; }
     }

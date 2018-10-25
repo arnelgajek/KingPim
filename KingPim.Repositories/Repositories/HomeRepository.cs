be359ace7a027681b444afcc9de1f608b2da8289
Arnel Gajek <gaar1719@student.ju.se>
2018-10-25 10:07:34 +0200

    Pulled the new Master.

---
+++ KingPim.Repositories/Repositories/HomeRepository.cs
@@ -0,0 +1,28 @@
+﻿using KingPim.Data;
+using KingPim.Models.Models;
+using KingPim.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KingPim.Repositories.Repositories
+{
+    public class HomeRepository : IHome
+    {
+        // Put the Db into a variable to use later on:
+        private ApplicationDbContext _ctx;
+        public HomeRepository(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public Category Get(int id)
+        {
+            return _ctx.Categories.FirstOrDefault(x => x.Id == id);
+        }
+        public IEnumerable<Category> GetAll()
+        {
+            return _ctx.Categories;
+        }
+    }
+}

c7fdd8106a7deaf568564672741f0f3ea96607e0
Arnel Gajek <gaar1719@student.ju.se>
2018-10-30 14:58:23 +0100

    Set the base for OneAttribute VC.

--- KingPim.Web/Components/OneAttributeCrudVc.cs
+++ KingPim.Web/Components/OneAttributeCrudVc.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KingPim.Models.ViewModels;
+using KingPim.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,18 @@ namespace KingPim.Web.Components
 {
     public class OneAttributeCrudVc : ViewComponent
     {
+        private IOneAttribute oneAttrRepo;
+
+        public OneAttributeCrudVc(IOneAttribute oneAttrRepository)
+        {
+            oneAttrRepo = oneAttrRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var oneAttrVm = new OneAttributeViewModel();
+
+            return View(oneAttrVm);
+        }
     }
 }

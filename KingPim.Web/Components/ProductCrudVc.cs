ceb5e903ec761caceb3bd57bcff73b4fb75bc708
Arnel Gajek <gaar1719@student.ju.se>
2018-10-30 11:23:31 +0100

    Worked on the ProductVc and its running smoothly for now.

--- KingPim.Web/Components/ProductCrudVc.cs
+++ KingPim.Web/Components/ProductCrudVc.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KingPim.Models.ViewModels;
+using KingPim.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,18 @@ namespace KingPim.Web.Components
 {
     public class ProductCrudVc : ViewComponent
     {
+        private IProduct prodRepo;
+
+        public ProductCrudVc(IProduct prodRepository)
+        {
+            prodRepo = prodRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var prodVm = new ProductViewModel();
+
+            return View(prodVm);
+        }
     }
 }

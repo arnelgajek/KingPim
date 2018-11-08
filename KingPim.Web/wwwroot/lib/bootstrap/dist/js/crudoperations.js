d6d299516608c5a2fcd8b4aef5924a48aad9b979
Arnel Gajek <gaar1719@student.ju.se>
2018-11-08 10:54:12 +0100

    Worked on the AttributeGroupCleanUp that contains adding a new attribute that doesn't exist in the list, all the things that should be displayed.

--- KingPim.Web/wwwroot/lib/bootstrap/dist/js/crudoperations.js
+++ KingPim.Web/wwwroot/lib/bootstrap/dist/js/crudoperations.js
@@ -1,6 +1,5 @@
 ﻿$(document).ready(function () {
 
-    // Allows the user to press Update and Delete when at least one checkbox i checked:
     var checkboxes = $(".chk");
 
     checkboxes.click(function () {
@@ -33,7 +32,6 @@
             dataType: 'JSON',
             success: function () {
                 console.log('JIPPIEEE!!!');
-                //location.reload();
             },
             error: function () {
                 console.log('NOOOO...');
@@ -44,8 +42,7 @@
 
     // CREATE:
     $('#createSomething').click(function () {
-
-        $('#createModal').on('show.bs.modal', function (event) {
+        $('#createModal').on('show.bs.modal', function () {
             var modal = $(this);
             modal.find('.modal-body select');
         });
@@ -55,7 +52,7 @@
     $('#updateSomething').click(function () {
         getValueUsingClass();
 
-        $('#updateModal').on('show.bs.modal', function (event) {
+        $('#updateModal').on('show.bs.modal', function () {
             var modal = $(this);
             var name = $(".table").find("input:checked").attr("data-name");
             modal.find('.modal-body p strong').text('You are about to update "' + name + '"');
@@ -89,4 +86,32 @@
 
         $('input[name="id"]').val(selected);
     }
+
+    $("#addAttributeBtn").on("click", function () {
+        $(".addAttribute").append(
+            "<div class='form-group' id='addAttributeForm'>" 
+            + "</br>"
+            + "<div class='form-group'>"
+            + "<input type='text' name='AttributeName' placeholder='Attribute Name...' class='form-control form-control-sm' /> </div>"
+            + "<div class='form-group'>"
+            + "<input type='text' name='Description' placeholder='Description...' class='form-control form-control-sm' /> </div>"
+            + "<div class='form-group'>"
+            + "<select class='form-control'>"
+            + "<option value='' disabled selected>Select a type</option>"
+            + "</br>"
+            + "<option>Number</option>"
+            + "<option>Text</option>"
+            + "<option>Checkbox</option>"
+            + "<option>Bool</option>"
+            + "</select>"
+            + "<button type='button' class='btn btn-success btn-sm' style='margin:5px'>Save</button>"
+            + "<button type='button' class='btn btn-danger btn-sm' id='deleteAttrBtn'>Delete</button>"
+            + "</div>"
+        );
+
+        $('#deleteAttrBtn').on("click", function () {
+            $(this).closest('#addAttributeForm').remove();
+            // After this function the user is not able to add a attribute again...
+        })
+    });
 });
